e14b895901d51d7adff89c247977c5a1d56103bf
pablolucio97 <pablolucio97@users.noreply.github.com>
2021-12-24 06:45:47 -0300

    Added MongoDb cluster and added each user authenticated to the collection

---
+++ src/services/MongoClient.ts
@@ -0,0 +1,22 @@
+import { Db, MongoClient } from 'mongodb';
+
+interface ConnectType{
+    dbConnect: Db;
+    client: MongoClient;
+}
+
+const uri = process.env.MONGODB_URI
+const dbName = process.env.MONGODB_DBNAME
+
+
+const client = new MongoClient(uri, {
+    //@ts-ignore
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+export default async function connectDb() : Promise<ConnectType>{
+    await client.connect()
+    const dbConnect = client.db(dbName)
+    return { dbConnect, client}
+}
